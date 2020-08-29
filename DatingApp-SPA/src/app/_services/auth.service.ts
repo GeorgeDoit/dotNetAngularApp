107903265bca71c03a41e19c2975a78b62839e4c
GeorgeDoit <aelgdd@gmail.com>
2020-08-29 13:41:25 +0300

    added register and home components

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+// this allows us to inject things to our service.We dont see it in components cause
+// they are by default Injectable.
+@Injectable({
+  // this tells our service which module providing this service
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  baseUrl = 'http://localhost:5000/api/auth/';
+
+  constructor(private http: HttpClient) { }
+
+  login(model) {
+    // this request returns a token. In order to do somehting with an observable when it comes back
+    // from a server we need to use RXJS operators, and to use the operators we need to pass them
+    // through a pipe method.
+    return this.http.post(this.baseUrl + 'login', model)
+      .pipe(
+        map((response: any) => {
+
+          const user = response;
+
+          if (user) {
+            localStorage.setItem('token', user.token);
+          }
+
+        })
+      );
+  }
+}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
+
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  // to store Username and Password
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  login() {
+
+    this.authService.login(this.model).subscribe(next => {
+
+      console.log('logged in successfully');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    // returns true or false value. If it has something return true else return false
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
 }
